1658345a5ca65188145a99750a13057f5bb54f90
JuanSebastianGB <3393@holbertonschool.com>
2022-04-03 21:54:19 +0000

    UPD: Finishing Header view

--- youtube-clone/src/Header.js
+++ youtube-clone/src/Header.js
@@ -10,17 +10,23 @@ import Avatar from '@material-ui/core/Avatar'
 export const Header = () => {
   return (
     <div className='header'>
-      <MenuIcon />
-      <img
-        className='header__logo'
-        src='https://upload.wikimedia.org/wikipedia/commons/3/3f/Logo_of_YouTube_%282005-2011%29.svg' alt=''
-      />
-      <input type='text' />
-      <SearchIcon />
-      <VideoCallIcon />
-      <AppsIcon />
-      <NotificationsIcon />
-      <Avatar src='https://avatars.githubusercontent.com/u/85469830?s=96&v=4' alt='' />
+      <div className="header__left">
+        <MenuIcon />
+        <img
+          className='header__logo'
+          src='https://upload.wikimedia.org/wikipedia/commons/3/3f/Logo_of_YouTube_%282005-2011%29.svg' alt=''
+        />
+      </div>
+      <div className="header__input">
+        <input placeholder='Search' type='text' />
+        <SearchIcon className='header__inputButton' />
+      </div>
+      <div className="header__icons">
+        <VideoCallIcon className='header__icon' />
+        <AppsIcon className='header__icon' />
+        <NotificationsIcon className='header__icon' />
+        <Avatar src='https://avatars.githubusercontent.com/u/85469830?s=96&v=4' alt='' />
+      </div>
     </div>
   );
 };
