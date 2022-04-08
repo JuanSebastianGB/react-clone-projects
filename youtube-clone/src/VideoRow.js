e75d4408fcc37f43ea05699b1fff928e0a6687b4
JuanSebastianGB <3393@holbertonschool.com>
2022-04-08 03:58:46 +0000

    Checking one more time to start to finish this project

--- youtube-clone/src/VideoRow.js
+++ youtube-clone/src/VideoRow.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import './VideoRow.css'
 
-export const VideoRow = () => {
+export const VideoRow = ({
+  views,
+  subs,
+  description,
+  timestamp,
+  channel,
+  title,
+  image
+}) => {
   return (
-    <div>VideoRow</div>
+    <div className='videoRow'>
+      <img src={image} alt="" />
+      <div className="videRow__text">
+        <h3>{title}</h3>
+        <p>
+          {channel} . {subs} subscribers {views} . {timestamp}
+        </p>
+      </div>
+    </div>
   );
 };
